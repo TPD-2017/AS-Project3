1641391dae752b78115bdbe7b373f109817b8cc8
dbastos10 <dani_b10_7@hotmail.com>
2016-05-07 18:24:11 +0100

    adjust clock fix.

--- src/tp3/clock/interfaces/AdjustClock.java
+++ src/tp3/clock/interfaces/AdjustClock.java
@@ -4,25 +4,22 @@ import tp3.clock.Clock;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Calendar;
 
 public class AdjustClock extends JPanel {
 
     private int currentSecond;
     private int currentMinute;
     private int currentHour;
+    Clock c;
 
-    public void pintar(Clock c, JTextArea textAreaH, JTextArea textAreaM, JTextArea textAreaS){
-        String h = textAreaH.getText();
-        String m = textAreaM.getText();
-        String s = textAreaS.getText();
-        if (!h.equals("") && !m.equals("") && !s.equals("")) {
-            currentHour = Integer.parseInt(h);
-            currentMinute = Integer.parseInt(m);
-            currentSecond = Integer.parseInt(s);
-            c.getState().adjustTime(currentSecond, currentMinute, currentHour, 0, 0, 0);
-        }
+    public void pintar(Clock clock){
         repaint();
         validate();
+        this.c = clock;
+        currentSecond = c.getCal().get(Calendar.SECOND);
+        currentMinute = c.getCal().get(Calendar.MINUTE);
+        currentHour = c.getCal().get(Calendar.HOUR_OF_DAY);
     }
 
     public void update(Graphics g)
@@ -42,10 +39,14 @@ public class AdjustClock extends JPanel {
 
         /*int currentSecond = Calendar.getInstance().get(Calendar.SECOND);
         int currentMinute = Calendar.getInstance().get(Calendar.MINUTE);
-        int currentHour = Calendar.getInstance().get(Calendar.HOUR);
+        int currentHour = Calendar.getInstance().get(Calendar.HOUR);*/
 
         g.setFont(new Font("TimesRoman", Font.PLAIN, 55));
         g.setColor(Color.black);
-        g.drawString(currentHour+" : "+currentMinute+" : "+currentSecond,135,255);*/
+        g.drawString(currentHour+" : "+currentMinute+" : "+currentSecond,135,255);
+
+        if (currentMinute==22){
+            this.c.getState().adjustTime(0, 0, 0, 0, 0, 0);
+        }
     }
 }
